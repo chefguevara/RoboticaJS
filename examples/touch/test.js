7be07f3b5d019076b946cb1f2576b97ce583925d
Marcos Tomatti <marcos.tomatti@cision.com>
2016-08-24 16:23:17 -0300

    Touch sensor

---
+++ examples/touch/test.js
@@ -0,0 +1,14 @@
+import five from 'johnny-five';
+
+let board = new five.Board();
+
+board.on("ready", () => {
+
+    let touch  = new five.Sensor.Digital(3);
+
+    touch.on('change', () => {
+        if (touch.value === 1) {
+            console.log('Keep your hands off!!!!');
+        }
+    });
+});
