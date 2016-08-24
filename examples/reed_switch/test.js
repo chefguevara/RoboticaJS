dc8004ef39145a77c9276fe5ba18df77290ed547
Marcos Tomatti <marcos.tomatti@cision.com>
2016-08-24 17:52:34 -0300

    Magnetic reed switch examples

---
+++ examples/reed_switch/test.js
@@ -0,0 +1,14 @@
+import five from 'johnny-five';
+
+let board = new five.Board();
+
+board.on("ready", () => {
+
+    let reed_switch  = new five.Sensor.Digital(3);
+
+    reed_switch.on('change', () => {
+        if (reed_switch.value === 1) {
+            console.log('I\'m feeling attracted!!!');
+        }
+    });
+});
