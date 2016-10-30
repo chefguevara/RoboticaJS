27f58de6a185a641fb1522a376f297fb4d086829
Marcos Tomatti <mtomatti-c@marchex.com>
2016-10-29 23:22:24 -0300

    playing with 3 buttons

--- examples/rgb_switch/test.js
+++ examples/rgb_switch/test.js
@@ -3,20 +3,64 @@ import five from 'johnny-five';
 let board = new five.Board();
 
 board.on('ready', () => {
-    let button = new five.Button({
-        pin: 3,
+    let red = new five.Button({
+            pin: 3
+        }),
+        green = new five.Button({
+            pin: 4
+        }),
+        blue = new five.Button({
+            pin: 5
+        }),
+        rgb = {
+            r: 0,
+            g: 0,
+            b: 0
+        };
+
+    function onRelease() {
+        console.log(`r: ${rgb.r}, g: ${rgb.g}, b: ${rgb.b}`);
+    }
+
+    red.on('hold', function onHold() {
+        if (rgb.r > 0) {
+            rgb.r--;
+        }
     });
 
-    button.on('hold', function onHold() {
-        console.log('Button held');
+    red.on('press', function onPress() {
+        if (rgb.r < 255) {
+            rgb.r++;
+        }
+    });
+    red.on('release', onRelease);
+
+
+    green.on('hold', function onHold() {
+        if (rgb.g > 0) {
+            rgb.g--;
+        }
     });
 
-    button.on('press', function onPress() {
-        console.log('Button pressed');
+    green.on('press', function onPress() {
+        if (rgb.g < 255) {
+            rgb.g++;
+        }
+    });
+    green.on('release', onRelease);
+
+
+    blue.on('hold', function onHold() {
+        if (rgb.b > 0) {
+            rgb.b--;
+        }
     });
 
-    button.on('release', function onRelease() {
-        console.log('Button released');
+    blue.on('press', function onPress() {
+        if (rgb.b < 255) {
+            rgb.b++;
+        }
     });
+    blue.on('release', onRelease);
 
 });
